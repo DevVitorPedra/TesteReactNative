cb466d4d399e55f51908a12420d165dbcaba2436
DevVitorPedra <75690532+DevVitorPedra@users.noreply.github.com>
2022-03-04 10:34:21 -0300

    window hook

--- TesteReactNative/src/components/background/index.js
+++ TesteReactNative/src/components/background/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import { StyledBackground } from './style'
+import { useWindowDimensions } from 'react-native'
 
 export default function ScrollBackground({children}) {
+  const windowWidth = useWindowDimensions().width
   return (
-    <StyledBackground  contentContainerStyle={{ alignItems: 'center' }}>{children}</StyledBackground>
+    <StyledBackground  contentContainerStyle={{ width:windowWidth, alignItems: 'center' }}>{children}</StyledBackground>
   )
 }
